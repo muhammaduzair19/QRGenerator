b4df3c8cc17ad84d29e0cc82b46fedf460f5839c
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2023-08-23 13:52:00 +0500

    download-functionality-added

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 // async function qrGenerator(){
 //     await fetch('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=facebook.com');
 // }
+
+
 let qrImage = document.getElementById('qr')
 let url = document.getElementById('url')
 let generate = document.getElementById('generate')
+let download = document.getElementById('download')
 
 
 function qrGenerator(text) {
-    qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`
+    try {
+        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`
+
+    } catch (error) {
+        console.log(error)
+    }
+
     qrImage.classList.add('show')
 }
 
 generate.addEventListener('click', () => {
+    console.log("working")
     if (url.value) {
         qrGenerator(url.value)
     }
@@ -20,7 +30,27 @@ generate.addEventListener('click', () => {
     }
 })
 
+download.addEventListener('click', downloadCode)
+
+async function downloadCode() {
+
+    try {
+        const response = await fetch(qrImage.src)
+        const file = await response.blob()
+        const link = document.createElement('a')
+        link.href = URL.createObjectURL(file)
+        link.download = new Date().getTime();
+        link.click();
+    }
+    catch (error) {
+
+    }
+}
+
+
+
 url.addEventListener('focus', () => {
     qrImage.src = "";
     qrImage.classList.remove('show')
 })
+
